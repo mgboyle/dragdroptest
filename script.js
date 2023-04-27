a880d372172805be020f3e1cd54141884bca8552
Micheal Boyle <michealboyle@outlook.com>
2023-04-27 13:24:39 -0400

    Updates to make it look nicer.

--- script.js
+++ script.js
@@ -2,23 +2,23 @@ const tasksData = [
     { id: 'task1', content: 'Creating presentations', app: 'app1' },
     { id: 'task2', content: 'Managing tasks', app: 'app2' },
     { id: 'task3', content: 'Editing photos', app: 'app3' },
-];
-
-const appsData = [
+  ];
+  
+  const appsData = [
     { id: 'app1', content: 'Microsoft PowerPoint' },
     { id: 'app2', content: 'Trello' },
     { id: 'app3', content: 'Adobe Photoshop' },
-];
-
-const tasks = document.getElementById('tasks');
-const apps = document.getElementById('apps');
-const scoreElement = document.createElement('h3');
-scoreElement.textContent = 'Score: 0';
-document.body.appendChild(scoreElement);
-
-let score = 0;
-
-tasksData.forEach(task => {
+  ];
+  
+  const tasks = document.getElementById('tasks');
+  const apps = document.getElementById('apps');
+  const scoreElement = document.createElement('h3');
+  scoreElement.textContent = 'Score: 0';
+  document.body.appendChild(scoreElement);
+  
+  let score = 0;
+  
+  tasksData.forEach(task => {
     const taskElement = document.createElement('div');
     taskElement.id = task.id;
     taskElement.className = 'task';
@@ -26,9 +26,9 @@ tasksData.forEach(task => {
     taskElement.draggable = true;
     taskElement.addEventListener('dragstart', onDragStart);
     tasks.appendChild(taskElement);
-});
-
-appsData.forEach(app => {
+  });
+  
+  appsData.forEach(app => {
     const appElement = document.createElement('div');
     appElement.id = app.id;
     appElement.className = 'app';
@@ -36,78 +36,114 @@ appsData.forEach(app => {
     appElement.addEventListener('dragover', onDragOver);
     appElement.addEventListener('drop', onDrop);
     apps.appendChild(appElement);
-});
-
-function onDragStart(event) {
+  });
+  
+  function onDragStart(event) {
     event.dataTransfer.setData('text/plain', event.target.id);
-}
-
-function onDragOver(event) {
+  }
+  
+  function onDragOver(event) {
     event.preventDefault();
-}
-
-function onDrop(event) {
+  }
+  
+  function onDrop(event) {
     const taskId = event.dataTransfer.getData('text/plain');
     const taskElement = document.getElementById(taskId);
     const targetAppElement = event.target;
-
+  
     const task = tasksData.find(t => t.id === taskId);
     if (task.app === targetAppElement.id) {
-        targetAppElement.classList.add('correct');
-        taskElement.style.display = 'none';
-        score++;
-        updateScore();
-        checkCompletion();
+      targetAppElement.classList.add('correct');
+      taskElement.style.display = 'none';
+      score++;
+      updateScore();
+      checkCompletion();
     } else {
-        alert('Incorrect match. Please try again.');
+      alert('Incorrect match. Please try again.');
     }
-
+  
     event.dataTransfer.clearData();
-}
-
-function updateScore() {
+  }
+  
+  function updateScore() {
     scoreElement.textContent = `Score: ${score}`;
-}
-
-// ... (rest of the script.js file)
-
-// Add an event listener for the submit button
-const submitButton = document.getElementById('submit-btn');
-submitButton.addEventListener('click', handleSubmit);
-
-function handleSubmit() {
-    let correctMatches = 0;
-
-    tasksData.forEach(task => {
-        const taskElement = document.getElementById(task.id);
-        const appElement = document.getElementById(task.app);
-
-        if (taskElement.parentNode === appElement) {
-            appElement.classList.add('correct');
-            correctMatches++;
-        } else {
-            appElement.classList.remove('correct');
-        }
-    });
-
-    setTimeout(() => {
-        const playerName = localStorage.getItem('playerName');
-        const leaderboard = JSON.parse(localStorage.getItem('leaderboard')) || [];
-        leaderboard.push({ name: playerName, score: correctMatches });
-        localStorage.setItem('leaderboard', JSON.stringify(leaderboard));
-
-        alert(`Congratulations ${playerName}! You matched ${correctMatches} out of ${tasksData.length} tasks correctly.`);
-    }, 500);
-}
-
-// ... (rest of the script.js file)
-
-// Add an event listener for the player form
-const playerForm = document.getElementById('player-form');
-playerForm.addEventListener('submit', (event) => {
+  }
+  
+  // Add a variable to track the start time
+  let startTime;
+  
+  // Add an event listener for the player form
+  const playerForm = document.getElementById('player-form');
+  playerForm.addEventListener('submit', (event) => {
     event.preventDefault();
     const playerName = document.getElementById('player-name').value;
     localStorage.setItem('playerName', playerName);
     playerForm.style.display = 'none';
     document.getElementById('game-container').style.display = 'block';
-});
+  
+    // Record the start time when the game starts
+    startTime = performance.now();
+  });
+  
+  function checkCompletion() {
+    const remainingTasks = tasksData.filter(task => document.getElementById(task.id).style.display !== 'none');
+    if (remainingTasks.length === 0) {
+      // All tasks are completed
+      const endTime = performance.now();
+      const timeTaken = Math.floor((endTime - startTime) / 1000); // Calculate time taken in seconds
+  
+      const playerName = localStorage.getItem('playerName');
+      const leaderboard = JSON.parse(localStorage.getItem('leaderboard')) || [];
+      leaderboard.push({ name: playerName, score: score, time: timeTaken });
+      localStorage.setItem('leaderboard', JSON.stringify(leaderboard));
+  
+      alert(`Congratulations ${playerName}! You matched all tasks correctly in ${timeTaken} seconds.`);
+  
+    // Redirect to the leaderboard page
+    window.location.href = 'leaderboard.html';
+  }
+}
+
+function populateLeaderboard() {
+  const leaderboardData = JSON.parse(localStorage.getItem('leaderboard')) || [];
+
+  leaderboardData.sort((a, b) => {
+    if (a.score !== b.score) {
+      return b.score - a.score;
+    } else if (a.time && b.time) {
+      return a.time - b.time;
+    } else {
+      return 0;
+    }
+  });
+
+  const tableBody = document.querySelector('#leaderboard tbody');
+
+  leaderboardData.forEach((entry, index) => {
+    const row = document.createElement('tr');
+
+    const rankCell = document.createElement('td');
+    rankCell.textContent = index + 1;
+    row.appendChild(rankCell);
+
+    const playerCell = document.createElement('td');
+    playerCell.textContent = entry.name;
+    row.appendChild(playerCell);
+
+    const scoreCell = document.createElement('td');
+    scoreCell.textContent = entry.score;
+    row.appendChild(scoreCell);
+
+    const timeCell = document.createElement('td');
+    timeCell.textContent = entry.time ? `${entry.time} s` : 'N/A';
+    row.appendChild(timeCell);
+
+    tableBody.appendChild(row);
+  });
+}
+
+// Call the populateLeaderboard function when the leaderboard page is loaded
+if (document.querySelector('#leaderboard')) {
+  populateLeaderboard();
+}
+  
